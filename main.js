4d1e12501b10f72e939f01e599a864f1b1373bb5
PabloCruz <pablocruzmiguez016@gmail.com>
2024-07-05 18:52:03 -0300

    Curso 3: Fin aula 2 y comienzo de aula 3

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.tecla_pom');
+function playSonidoPom(){
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+function playSonidoClap() {
+    document.querySelector('#sonido_tecla_clap').play()
+}
+// Para las clases utilizamos el . (punto) y para los identidicadores usamos # (id)
+// NO se deben poner los parentesis
+ document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
